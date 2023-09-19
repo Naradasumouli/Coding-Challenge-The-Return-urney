12eb7a06ea7cb81570e9e0ca9778c127b24917b1
Naradasumouli <131344304+Naradasumouli@users.noreply.github.com>
2023-09-19 14:32:49 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  databaseURL: 'mongodb://localhost/your-database-name',
+  ipinfoAPIKey: 'your-ipinfo-api-key',
+  twilioAccountSid: 'your-account-sid',
+  twilioAuthToken: 'your-auth-token',
+  twilioPhoneNumber: 'your-twilio-phone-number',
+  databaseURL: 'mongodb://localhost/your-database-name',
+  saltRounds: 10, // Number of bcrypt salt rounds
+};
